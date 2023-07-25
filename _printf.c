66b2307cb8a2d107ec7f885fdb51ac643e646a66
ayoubweb <ayoubweb02@gmail.com>
2023-07-26 00:02:06 +0100

    update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,57 +1,54 @@
 #include "main.h"
 #include <unistd.h>
-
 /**
- * _printf -  produces output according to a format,
- * write output to stdout, the standard output stream
- * @format: 
+ * _printf -  print a formated string
+ * @format: pointer to a string containing all the desired characters
  *
  * Return:  number of characters printed
 */
 int _printf(const char *format, ...)
 {
-	int i = 0, len = 0;
-	char *str;
+	int i = 0, j = 0, len = 0, r_val;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
 	va_list args;
 
 	if (format == NULL)
 		return (-1);
-
 	va_start(args, format);
-
-	while (format[i])
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
-			i++;
-			switch (format[i])
+			for (j = 0; f_list[j].sym != NULL; j++)
 			{
-			case 'c':
-				len += _putchar(va_arg(args, int));
-				break;
-			case 's':
-			{
-				str = va_arg(args, char *);
-				while (*str)
+				if (format[i + 1] == f_list[j].sym[0])
 				{
-					if (!str)
-						len = write(1, "(null)", 6);
-					len += _putchar(*str);
-					str++;
+					r_val = f_list[j].f(args);
+					if (r_val == -1)
+						return (-1);
+					len += r_val;
+					break;
 				}
-				break;
 			}
-			case '%':
-				len += _putchar('%');
-				break;
-			default:
-				len += _putchar(format[i]);
-				break;
+			if (f_list[j].sym == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != '\0')
+				{
+					len += _putchar(format[i]);
+					len += _putchar(format[i + 1]);
+				}
+				else
+					return (-1);
 			}
+			i++;
 		}
 		else
 			len += _putchar(format[i]);
-		i++;
 	}
 	va_end(args);
 	return (len);
