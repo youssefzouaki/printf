12c406ca5914f5e6db6365d19107d1ffb91016db
youssefzouaki <youssefzouaki@gmail.com>
2023-07-26 02:36:00 +0100

    updat _printf.c

--- _printf.c
+++ _printf.c
@@ -14,7 +14,13 @@ int _printf(const char *format, ...)
 		{"%", print_percent},
 		{"d", print_integer},
 		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
 		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
 		{NULL, NULL}
 	};
 	va_list args;
